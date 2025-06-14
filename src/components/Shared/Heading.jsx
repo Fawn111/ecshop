c65f4e448b72acfa6bdb070fd069b9f98e9794f0
Moiz <rmoiz0002@gmail.com>
2025-06-15 02:29:59 +0500

    first commit

--- src/components/Shared/Heading.jsx
+++ src/components/Shared/Heading.jsx
@@ -6,7 +6,6 @@ const Heading = ({ subtitle , title }) => {
     <div className='text-center space-y-3'>
         <h2 className='text-4xl font-bold'>{title}</h2>
         <p className='text-l text-gray-500'>{subtitle}</p>
-        <Button text="Visit Our Youtube Channel"/>
     </div>
   )
 }
